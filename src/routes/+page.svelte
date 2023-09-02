4513d809c3c01c3495ba04297e8d7f66d57343bd
MatthiasChometon <matthiaschometon787@gmail.com>
2023-09-02 16:26:34 +0200

    feat: web test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,10 @@
 
 		peer.on('open', function (id) {
 			console.log('My peer ID is: ' + id);
+
+			peer.on('connection', function (conn) {
+				console.log('connect another peer');
+			});
 		});
 	});
 
