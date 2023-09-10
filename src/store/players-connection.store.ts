e605fcd0da4331c8fd07d9e74310f402050429d1
MatthiasChometon <matthiaschometon787@gmail.com>
2023-09-10 15:02:28 +0200

    feat: add sendData

--- src/store/players-connection.store.ts
+++ src/store/players-connection.store.ts
@@ -19,24 +19,23 @@ export function PlayersConnection<T> (onDataReceived: (newData: T) => void) {
 	}
 
 	function parseReceivedData<T> (newData: unknown): T {
-		console.log(JSON.parse(String(newData)));
 		return JSON.parse(String(newData))
 	}
 
 	function openConnection (connection: DataConnection): void {
 		connection.on('open', function () {
+			console.log('player joined your game')
 			update(state => ({ ...state, isConnectedToOpponent: true }));
 
 			connection.on('data', function (data: unknown) {
+				console.log('Received', data);
 				const parsedData: T = parseReceivedData(data);
 				onDataReceived(parsedData);
 			});
 		});
 	}
 
-	async function openGame (peer: Peer): Promise<void> {
-		peer = await getNewPeer();
-
+	function openGame (peer: Peer): void {
 		peer.on('open', function (playerPeerId: string) {
 			update(state => ({ ...state, playerPeerId }));
 
@@ -54,22 +53,33 @@ export function PlayersConnection<T> (onDataReceived: (newData: T) => void) {
 			const { peer, opponentPeerId } = state;
 			if (opponentPeerId === null || peer === null) return state;
 			const connection = peer.connect(opponentPeerId);
+			console.log('connected to player game')
 			openConnection(connection);
 			return { ...state, connection }
 		}
 		);
 	}
 
+	function sendData (data: T): void {
+		update(state => {
+			const { connection } = state;
+			if (connection === null) return state;
+			connection.send(data);
+			return state;
+		});
+	}
+
 	return {
 		subscribe,
 		set,
-		connectToCreatedGame: () => {
-			connectToCreatedGame();
-		},
+		connectToCreatedGame,
 		createNewGame: async () => {
 			const peer = await getNewPeer();
 			update(state => ({ ...state, peer }));
 			openGame(peer);
+		},
+		sendData: (data: T) => {
+			sendData(data)
 		}
 	};
 };
