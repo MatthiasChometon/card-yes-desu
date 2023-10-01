0d7f2b0cb6f462ac527936bbcdd73be3a2458e81
MatthiasChometon <matthiaschometon787@gmail.com>
2023-10-01 10:33:03 +0200

    feat: no delete focus for pc version

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -3,6 +3,7 @@
 	import { currentCardHover } from '../store/current-card-hover.store';
 	import type { ClientPosition } from '../types/client-position';
 	import type { PlayableCard } from '../types/playable-card.type';
+	import MediaQuery from './media-query.svelte';
 
 	export let card: PlayableCard,
 		style: string = '',
@@ -12,17 +13,18 @@
 	$: pictureDisplayed = card.gameState.faceUp ? card.frontPicture : card.backPicture;
 </script>
 
-<div
-	style="width: 100%; height: 100%; {style}"
-	on:mouseover={() => currentCardHover.set({ ...card, gameState: { ...card.gameState, rotation: 0 } })}
-	on:focus={() => currentCardHover.set({ ...card, gameState: { ...card.gameState, rotation: 0 } })}
-	on:blur={() => currentCardHover.set(null)}
-	on:mouseleave={() => currentCardHover.set(null)}
-	tabindex="0"
-	role="button"
->
+<MediaQuery query="(max-width: 800px)" let:matches>
 	<div
-		style="
+		style="width: 100%; height: 100%; {style}"
+		on:mouseover={() => currentCardHover.set({ ...card, gameState: { ...card.gameState, rotation: 0 } })}
+		on:focus={() => currentCardHover.set({ ...card, gameState: { ...card.gameState, rotation: 0 } })}
+		on:blur={() => (matches ? currentCardHover.set(null) : null)}
+		on:mouseleave={() => (matches ? currentCardHover.set(null) : null)}
+		tabindex="0"
+		role="button"
+	>
+		<div
+			style="
 		background-image: url({pictureDisplayed});
 		background-size: contain;
 		background-repeat: no-repeat;
@@ -33,8 +35,9 @@
 		transform: rotate({card.gameState.rotation}deg);
 		{pictureStyle}
 	"
-		role="button"
-		tabindex="0"
-		on:contextmenu|preventDefault={onRightClick}
-	/>
-</div>
+			role="button"
+			tabindex="0"
+			on:contextmenu|preventDefault={onRightClick}
+		/>
+	</div>
+</MediaQuery>
